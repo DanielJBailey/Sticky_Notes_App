a21a1b48f104d3e248a1f8fa63455f2e913206fe
Daniel Bailey <danielbailey@Daniels-MacBook-Pro.local>
2018-11-12 23:43:24 -0700

    add more styling, add create new sticky page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,42 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(notes_params)
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+    # render :edit
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  private
+    def notes_params
+      params.require(:note).permit(:body)
+    end
 end
+
+
+
+
+
